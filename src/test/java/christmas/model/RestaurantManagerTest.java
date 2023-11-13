277dee663551d6add78ac8aab063df3a4de54549
seondays <agseon9@gmail.com>
2023-11-13 20:20:19 +0900

    test: 12월 이벤트 평일 주말 할인 혜택 적용을 위해 메뉴 개수 체크하는 기능 테스트

---
+++ src/test/java/christmas/model/RestaurantManagerTest.java
@@ -0,0 +1,29 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class RestaurantManagerTest {
+
+    private RestaurantManager restaurantManager;
+    private static Map<Menu, Integer> orderedMenu;
+
+    @BeforeEach
+    void init() {
+        orderedMenu = Map.of(Menu.TBONE_STEAK,4,
+                Menu.CHOCOLATE_CAKE,1,Menu.CHRISTMAS_PASTA,1,Menu.ICE_CREAM,1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("주어진 날짜에 따라 주문한 메뉴 중 몇가지 메뉴를 할인할지 알아낸다")
+    @CsvSource(value = {"3:2", "15:5"}, delimiter = ':')
+    void checkWeekOrWeekend(int days, int week) {
+        restaurantManager = new RestaurantManager(days, orderedMenu);
+        assertThat(restaurantManager.countDiscountMenu()).isEqualTo(week);
+    }
+}
