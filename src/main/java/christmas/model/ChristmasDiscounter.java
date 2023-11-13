9e4fd210c6d721c07d119afb6470dc40223c6d3c
seondays <agseon9@gmail.com>
2023-11-13 21:47:42 +0900

    feat: 크리스마스 디데이 할인 기능 구현

---
+++ src/main/java/christmas/model/ChristmasDiscounter.java
@@ -0,0 +1,33 @@
+package christmas.model;
+
+public class ChristmasDiscounter {
+    private static final int LIMIT_DAY = 25;
+    private static final int DEFAULT_AMOUNT = 1000;
+    private static final int EVERY_DAY_ADD_AMOUNT = 100;
+    private static final int DAY_CONTROL = 1;
+    private final int day;
+    private final int cost;
+    private int resultAmount;
+
+    public ChristmasDiscounter(int day, int cost) {
+        this.day = day;
+        this.cost = cost;
+        setResultAmount();
+    }
+
+    public int discountCost() {
+        return cost - resultAmount;
+    }
+
+    public int getResultAmount() {
+        return resultAmount;
+    }
+
+    private void setResultAmount() {
+        if (day <= LIMIT_DAY) {
+            resultAmount = DEFAULT_AMOUNT + ((day - DAY_CONTROL) * EVERY_DAY_ADD_AMOUNT);
+            return;
+        }
+        resultAmount = 0;
+    }
+}
