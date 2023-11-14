99432381dc40606e7a967782198fdb5c1fcc8043
seondays <agseon9@gmail.com>
2023-11-14 22:26:18 +0900

    feat: 문자열 가공하는 기능을 하는 parser 구현

---
+++ src/main/java/christmas/utils/Parser.java
@@ -0,0 +1,19 @@
+package christmas.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+    private final static String INPUT_REGEX = ",";
+    private final static String MENU_REGEX = "-";
+
+    public static List<String> splitInput(String input) {
+        return Arrays.stream(input.split(INPUT_REGEX))
+                .map(String::trim)
+                .toList();
+    }
+
+    public static String[] splitMenu(String menu) {
+        return menu.split(MENU_REGEX);
+    }
+}
