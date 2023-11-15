0ebb79acf33071e6f0fbc99dc997501f9ec2b68a
seondays <agseon9@gmail.com>
2023-11-15 18:42:36 +0900

    refactor: 컨트롤러 전역에서 사용되는 클래스 의존성 주입하도록 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,15 @@
 package christmas;
 
 import christmas.controller.Controller;
+import christmas.model.StringMaker;
+import christmas.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-       Controller controller = new Controller();
+        InputView inputView = new InputView();
+        StringMaker stringMaker = new StringMaker();
+
+        Controller controller = new Controller(stringMaker, inputView);
         controller.run();
     }
 }
