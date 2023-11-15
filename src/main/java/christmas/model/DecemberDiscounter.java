f3e385d74cc5a3150917c7459079dddb211177c6
seondays <agseon9@gmail.com>
2023-11-15 18:10:08 +0900

    refactor: 12월 이벤트 할인 관련 클래스 조정

--- src/main/java/christmas/model/DecemberDiscounter.java
+++ src/main/java/christmas/model/DecemberDiscounter.java
@@ -1,13 +1,24 @@
 package christmas.model;
 
-public class DecemberDiscounter implements Discounter{
+import christmas.utils.constant.DiscountMessage;
+import christmas.utils.constant.DiscounterConstant;
+import christmas.utils.constant.Menu;
+import java.time.LocalDate;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class DecemberDiscounter implements Discounter {
     private static final int DISCOUNT_AMOUNT = 2023;
+    private int day;
+    private Map<Menu, Integer> orderedMenu;
     private final int resultAmount;
     private final int cost;
 
-    public DecemberDiscounter(int cost, int menuCount) {
-        resultAmount = menuCount * DISCOUNT_AMOUNT;
+    public DecemberDiscounter(int day, int cost, Map<Menu, Integer> orderedMenu) {
+        this.day = day;
         this.cost = cost;
+        this.orderedMenu = orderedMenu;
+        resultAmount = countDiscountMenu() * DISCOUNT_AMOUNT;
     }
 
     @Override
@@ -18,5 +29,47 @@ public class DecemberDiscounter implements Discounter{
     @Override
     public int getResultAmount() {
         return resultAmount;
+    }
+
+    @Override
+    public String getDiscountMessage() {
+        if (isDayWeekend()) {
+            return DiscountMessage.WEEKEND.getMessage();
+        }
+        return DiscountMessage.WEEK.getMessage();
+    }
+
+    ;
+
+    // 날짜에 따라 메뉴에서 몇개를 할인해야 할지 알려줘
+    public int countDiscountMenu() {
+        if (isDayWeekend()) {
+            return countMainMenu();
+        }
+        return countDessertMenu();
+    }
+
+    // 날짜가 평일인지 주말인지 알려줘
+    private boolean isDayWeekend() {
+        LocalDate date = LocalDate.of(DiscounterConstant.YEAR.getNumber(), DiscounterConstant.MONTH.getNumber(), day);
+        int dateNumber = date.getDayOfWeek().getValue();
+        return dateNumber == DiscounterConstant.SATURDAY_MARK.getNumber()
+                || dateNumber == DiscounterConstant.FRIDAY_MARK.getNumber();
+    }
+
+    // 메뉴에서 디저트가 몇갠지 알려줘
+    private int countDessertMenu() {
+        return orderedMenu.entrySet().stream()
+                .filter(entry -> entry.getKey().getMenuType().equals("dessert"))
+                .mapToInt(EnumMap.Entry::getValue)
+                .sum();
+    }
+
+    // 메뉴에서 메인이 몇갠지 알려줘
+    private int countMainMenu() {
+        return orderedMenu.entrySet().stream()
+                .filter(entry -> entry.getKey().getMenuType().equals("main"))
+                .mapToInt(Map.Entry::getValue)
+                .sum();
     }
 }
