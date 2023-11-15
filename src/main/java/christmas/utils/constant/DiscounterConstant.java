d8459071c8ca0b88da8947ca9f2541bb2392d192
seondays <agseon9@gmail.com>
2023-11-15 14:38:30 +0900

    refactor: enum명 변경 및 값 추가
    DiscounterConstant로 클래스명 변경, 값 추가

--- src/main/java/christmas/utils/constant/DiscounterConstant.java
+++ src/main/java/christmas/utils/constant/DiscounterConstant.java
@@ -1,16 +1,18 @@
 package christmas.utils.constant;
 
-public enum SpecialDiscounterConstant {
+public enum DiscounterConstant {
     YEAR(2023),
     MONTH(12),
     SUNDAY_MARK(7),
+    FRIDAY_MARK(5),
+    SATURDAY_MARK(6),
     CHRISTMAS(25),
     DISCOUNT_DEFAULT_AMOUNT(1_000),
     MINIMUM_DISCOUNT_COST(10_000);
 
     private final int number;
 
-    SpecialDiscounterConstant(int number) {
+    DiscounterConstant(int number) {
         this.number = number;
     }
 
