b2af42346859eabc5918bf8ac480ecfb4940b1e8
seondays <agseon9@gmail.com>
2023-11-15 18:13:00 +0900

    test: 12월 이벤트 할인 관련 클래스 조정으로 테스트 코드 수정

--- src/test/java/christmas/model/DecemberDiscounterTest.java
+++ src/test/java/christmas/model/DecemberDiscounterTest.java
@@ -2,26 +2,46 @@ package christmas.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.utils.constant.Menu;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class DecemberDiscounterTest {
     private DecemberDiscounter decemberDiscounter;
+    private DecemberDiscounter restaurantManager;
+    private static Map<Menu, Integer> orderedMenu;
 
-    @ParameterizedTest
-    @CsvSource(value = {"10000:3:3931", "20000:6:7862"}, delimiter = ':')
-    @DisplayName("총 금액과 할인할 메뉴의 개수를 전달하면 메뉴당 2023만큼 할인해 주는지 테스트")
-    void discountCostRight(int cost, int menuCount, int expected) {
-        decemberDiscounter = new DecemberDiscounter(cost, menuCount);
-        assertThat(decemberDiscounter.discountCost()).isEqualTo(expected);
+    @BeforeEach
+    void init() {
+        orderedMenu = Map.of(Menu.TBONE_STEAK, 4,
+                Menu.CHOCOLATE_CAKE, 1, Menu.CHRISTMAS_PASTA, 1, Menu.ICE_CREAM, 1);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"10000:3:6069", "20000:6:12138"}, delimiter = ':')
-    @DisplayName("얼마를 할인받았는지를 잘 반환하는지 테스트")
-    void getDiscountAmount(int cost, int menuCount, int expected) {
-        decemberDiscounter = new DecemberDiscounter(cost, menuCount);
+    @DisplayName("주어진 날짜에 따라 주문한 메뉴 중 몇가지 메뉴를 할인할지 알아낸다")
+    @CsvSource(value = {"3:2", "15:5"}, delimiter = ':')
+    void checkWeekOrWeekend(int days, int week) {
+        restaurantManager = new DecemberDiscounter(days, 10000, orderedMenu);
+        assertThat(restaurantManager.countDiscountMenu()).isEqualTo(week);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"3:270000:265954","15:270000:259885"}, delimiter = ':')
+    @DisplayName("주어진 날짜에 따라 올바르게 할인되는지 테스트")
+    void discountCostRight(int days, int cost, int expected) {
+        decemberDiscounter = new DecemberDiscounter(days, cost, orderedMenu);
+        assertThat(decemberDiscounter.discountCost()).isEqualTo(expected);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"2:100000:10115", "26:100000:4046"}, delimiter = ':')
+    @DisplayName("얼마를 할인받았는지를 반환하는지 테스트")
+    void getDiscountAmount(int days, int menuCount, int expected) {
+        decemberDiscounter = new DecemberDiscounter(days, menuCount, orderedMenu);
         assertThat(decemberDiscounter.getResultAmount()).isEqualTo(expected);
     }
 }
