7f044cff891811700d3d35fefa70e56000ea7289
seondays <agseon9@gmail.com>
2023-11-15 16:45:07 +0900

    fix: 오타 수정

--- src/main/java/christmas/utils/constant/DiscountMessage.java
+++ src/main/java/christmas/utils/constant/DiscountMessage.java
@@ -4,7 +4,7 @@ public enum DiscountMessage {
     GIFT_EVENT("증정 이벤트: "),
     MONEY_UNIT("원\n"),
     NONE("없음\n"),
-    CHRISTMAS("\"크리스마스 디데이 할인: \""),
+    CHRISTMAS("크리스마스 디데이 할인: "),
     SPECIAL("특별 할인: "),
     WEEK("평일 할인: "),
     WEEKEND("주말 할인: ");
